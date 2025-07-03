7c86488878ba7a3ce0f93d8c370362224fd84049
vishnukoushikkumara <vishnukoushik.707@gmail.com>
2025-07-03 19:25:20 +0530

    Fix React routing

--- backend/routes/LoginRoute.js
+++ backend/routes/LoginRoute.js
@@ -43,6 +43,42 @@ router.post("/register/", async (request, response) => {
   }
 });
 
+router.post("/register", async (request, response) => {
+  const { username, password, email } = request.body;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const dbUsername = await User.find({ Username: username });
+    const dbEmail = await User.find({ Email: email });
+    if (dbUsername.length === 0 && dbEmail.length === 0) {
+      if (password.length < 6) {
+        return response.status(400).json({ error: "Password is too short" });
+      } else {
+        const newUser = new User({
+          Username: username,
+          HashedPassword: hashedPassword,
+          email: email,
+        });
+        const dbResponse = await User.create(newUser);
+        // Create JWT token
+        const payload = {
+          username: username,
+          user_id: dbResponse._id.toString(),
+        };
+        const jwtoken = jwt.sign(payload, "MY_SECRET_TOKEN");
+        return response.status(200).json({ token: jwtoken });
+      }
+    } else {
+      if (dbUsername.length !== 0) {
+        return response.status(400).json({ error: "Username already exists" });
+      } else if (dbEmail.length !== 0) {
+        return response.status(400).json({ error: "Email already exists" });
+      }
+    }
+  } catch (err) {
+    return response.status(500).json({ error: "Internal server error" });
+  }
+});
+
 router.post("/login/", async (req, res) => {
   const { username, password } = req.body;
   const dbuser = await User.findOne({ Username: username }); // Changed to findOne
